6fc1982efd5ca515d0da7bee1625159b214a54b0
QinYang Tan <108197858+TanQinYang@users.noreply.github.com>
2023-10-30 22:20:37 +0100

    Add files via upload

---
+++ head File/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main()
+{
+/*    time_t*t=time(NULL);
+    localtime(t);
+    srand((unsigned)time(NULL));*/
+//   int x=rand()%1000;
+//    srand(52);
+   // int x=rand()%101;
+   // printf("%d",x);
+ //   system()
+//    system("cmd");
+//    system("notebook");
+    system("shutdown -p");
+    return 0;
+ ;
+}
